32419e51bfcb5caa447dcd1b6fb85519716119fe
Quoc Thinh <lquocthinh10a1@gmail.com>
2023-01-31 21:34:11 +0700

    Config db and login logout, dont test yet

--- app.py
+++ app.py
@@ -1,9 +1,67 @@
-from flask import Flask , render_template, url_for, redirect
-
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
-if __name__ =='__main__': 
-    app.run()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///do_an1.db'
+app.config['MYSQL_HOST'] = 'localhost:3306'
+app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+db = SQLAlchemy(app)
+
+# app.secret_key = "secret_key"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            session["username"] = username
+            return redirect(url_for("dashboard"))
+        else:
+            return "Wrong credentials. Try again."
+    return render_template("login.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("signup.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "username" in session:
+        username = session["username"]
+        return render_template("dashboard.html", username=username)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
